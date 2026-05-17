8f0da8f00ae1bd28990884fce223ac0a3d1e313f
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2026-05-17 00:33:15 -0300

    feat: add nest config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
@@ -9,6 +10,7 @@ import { RebalancingModule } from '@/modules/rebalancing/rebalancing.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     BasketModule,
     QuoteModule,
